76147e52e13ff428c8a977b0263e7becb7072f41
BuiDuc1812 <db939397@gmail.com>
2022-07-30 08:23:36 +0700

    first commit

---
+++ BuiDuc2019602066_BanNoiThat/pages/main/contact.php
@@ -0,0 +1,29 @@
+<style>
+     .contacts{
+         text-align: center; 
+         font-size: 18px;   
+     }
+ </style>
+ <h2>LIÊN HỆ</h2>
+<div class="contacts">
+ <p>Facebook : <a href="https://www.facebook.com/bduccc.12/"> Bấm vào đây</a></p>
+ <p>SĐT : 03866075101</p>
+
+ <div class="ceo-grid">
+                    <div class="ceo">
+                        <img src="https://i1.wp.com/netizen-kore.com/wp-content/uploads/2021/06/nawk6v4uyzfdpads3mbvfvauk4.jpg?resize=400%2C499&ssl=1" alt="" class="img-ceo">
+                        <h3>Bùi Văn Đức</h3>
+                        <p>CEO</p>
+                    </div>
+                    <div class="ceo">
+                        <img src="https://i1.wp.com/netizen-kore.com/wp-content/uploads/2021/06/nawk6v4uyzfdpads3mbvfvauk4.jpg?resize=400%2C499&ssl=1" alt="" class="img-ceo">
+                        <h3>Lã Lê Duy</h3>
+                        <p>CEO</p>
+                    </div>
+                    <div class="ceo">
+                        <img src="https://i1.wp.com/netizen-kore.com/wp-content/uploads/2021/06/nawk6v4uyzfdpads3mbvfvauk4.jpg?resize=400%2C499&ssl=1" alt="" class="img-ceo">
+                        <h3>Phan Minh Giang</h3>
+                        <p>CEO</p>
+                    </div>
+                </div>
+ </div>
